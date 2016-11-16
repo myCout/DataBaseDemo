5a8f8104f4000a01640cd071c1ca25fc6c56d81f
NickHao <491493118@qq.com>
2016-11-16 11:01:20 +0800

    Initial Commit

---
+++ DataBaseDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DataBaseDemo
+//
+//  Created by HY on 2016/11/16.
+//  Copyright © 2016年 HY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DataBaseDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DataBaseDemo
+//
+//  Created by HY on 2016/11/16.
+//  Copyright © 2016年 HY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
